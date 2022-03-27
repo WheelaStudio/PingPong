66de5c2b940aa2baf9fc6eee9601ddcbd6346055
WheelaStudio <wheelastudio@gmail.com>
2022-03-27 13:36:16 +0300

    game mechanic update

--- Assets/Scripts/Game/BallController.cs
+++ Assets/Scripts/Game/BallController.cs
@@ -5,6 +5,7 @@ public class BallController : MonoBehaviour
     [SerializeField] private AudioSource collisonSoundSource;
     [SerializeField] private float maxVelocity, minXAxisVelocity, minYAxisVelocity;
     private Rigidbody2D body;
+    private WaitForSeconds delay = new(1f);
     private void Start()
     {
         body = GetComponent<Rigidbody2D>();
@@ -16,25 +17,26 @@ public class BallController : MonoBehaviour
     }
     private IEnumerator OnTriggerEnter2D(Collider2D collision)
     {
-        yield return new WaitForSeconds(1f);
+        yield return delay;
         body.position = Vector2.zero;
         body.velocity = Vector2.zero;
         StartMove();
     }
     private void StartMove()
     {
-        body.AddForce(new Vector2(Random.Range(0, 2) == 0 ? -0.1f : 0.1f, Random.Range(0, 2) == 0 ? Random.Range(-0.25f, -0.1f) : Random.Range(0.1f, 0.25f)), ForceMode2D.Impulse);
+        body.AddForce(new Vector2(Random.Range(0, 2) == 0 ? 0.1f : -0.1f, Random.Range(0, 2) == 0 ? Random.Range(0.1f, 0.15f) :
+            Random.Range(-0.15f, -0.1f)), ForceMode2D.Impulse);
     }
     private void FixedUpdate()
     {
         var velocity = body.velocity;
         if (velocity.x < minXAxisVelocity && velocity.x > 0f)
             velocity.x = minXAxisVelocity;
-        if (velocity.x > -minXAxisVelocity && velocity.x < 0f)
+        if (velocity.x > -minXAxisVelocity && velocity.x <= 0f)
             velocity.x = -minXAxisVelocity;
         if (velocity.y < minYAxisVelocity && velocity.y > 0f)
             velocity.y = minYAxisVelocity;
-        if (velocity.y > -minYAxisVelocity && velocity.y < 0f)
+        if (velocity.y > -minYAxisVelocity && velocity.y <= 0f)
             velocity.y = -minYAxisVelocity;
         velocity = Vector2.ClampMagnitude(velocity, maxVelocity);
         body.velocity = velocity;
