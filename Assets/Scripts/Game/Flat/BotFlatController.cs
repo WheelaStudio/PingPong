b242ad85fed1947df2c25c93f0ab141a0733a457
WheelaStudio <wheelastudio@gmail.com>
2022-04-11 13:56:54 +0300

    bot mechanic update

--- Assets/Scripts/Game/Flat/BotFlatController.cs
+++ Assets/Scripts/Game/Flat/BotFlatController.cs
@@ -3,7 +3,7 @@ public class BotFlatController : Flat
 {
     private Rigidbody2D ballBody;
     private BallController ballController;
-    private (float, float) speedSpread = (0.04f, 0.1f);
+    private (float, float) speedSpread = (0.007f, 0.02885f);
     protected override void Awake()
     {
         screenSide = Preferences.PlayerSide == ScreenSide.Left ? ScreenSide.Right : ScreenSide.Left;
@@ -13,21 +13,15 @@ public class BotFlatController : Flat
     {
         ballController = BallController.Shared;
         ballBody = ballController.GetComponent<Rigidbody2D>();
-        var aspectRatio = Preferences.DistanceBetweenFlats / Preferences.ScreenWorldHeight;
-        speedSpread.Item1 *= aspectRatio;
-        speedSpread.Item2 *= aspectRatio;
         if (screenSide == ScreenSide.Right)
             transform.eulerAngles = new Vector3(0f, 0f, 180f);
-        var speed = ballController.Speed;
-        speedSpread.Item1 /= speed;
-        speedSpread.Item2 /= speed;
         base.Start();
     }
     private void FixedUpdate()
     {
         if (!ballController.IsOnTheField) return;
         var newPosition = body.position + new Vector2(0f, (ballBody.position.y < body.position.y ? -1f : 1f) 
-          * Random.Range(speedSpread.Item1, speedSpread.Item2) * ballController.Speed);
+          * Random.Range(speedSpread.Item1, speedSpread.Item2) * Mathf.Abs(ballBody.velocity.y));
         if (newPosition.y < yBottomCoordinate || newPosition.y > yTopCoordinate) return;
         body.MovePosition(newPosition);
     }
